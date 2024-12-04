553cd9a7503a7d79d34682ffc0c4b20c36b9f000
Harendharan B <93971342+Harendharan@users.noreply.github.com>
2024-12-05 00:51:21 +0530

    Update and rename Instruction and Data Mem to Instruction and Data Memory/imem.v

---
+++ Instruction and Data Memory/imem.v
@@ -0,0 +1,14 @@
+module imem (
+    input [31:0] a,
+    output reg [31:0] rd
+);
+    
+   reg [31:0] RAM[63:0]; // 32 X 64
+
+    initial begin
+         $readmemh("memfile.dat", RAM);
+         rd = RAM[a[31:2]]; 
+
+    end
+
+endmodule
