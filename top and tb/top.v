3de57bce29b921b8c234aef44da9c147d4653bea
Harendharan B <93971342+Harendharan@users.noreply.github.com>
2024-12-05 12:24:52 +0530

    Update top.v

--- top and tb/top.v
+++ top and tb/top.v
@@ -2,41 +2,17 @@
 `include "imem.v"
 `include "dmem.v"
 
-module top (
-    input clk, 
-    input reset
-);
+module top (input clk, input reset);
 
-    // Internal wires for connecting modules
+    // Internal wires
     wire [31:0] PC, Instr, ReadData;
     wire [31:0] WriteData, DataAdr;
     wire MemWrite;
 
-    // Instantiate the CPU
-    cpu c1 (
-        .clk(clk), 
-        .reset(reset), 
-        .PC(PC), 
-        .Instr(Instr), 
-        .MemWrite(MemWrite), 
-        .ALUResult(DataAdr), 
-        .WriteData(WriteData), 
-        .ReadData(ReadData)
-    );
-
-    // Instantiate the Instruction Memory (IMEM)
-    imem c2 (
-        .a(PC), 
-        .rd(Instr)
-    );
-
-    // Instantiate the Data Memory (DMEM)
-    dmem c3 (
-        .clk(clk), 
-        .we(MemWrite), 
-        .a(DataAdr), 
-        .wd(WriteData), 
-        .rd(ReadData)
-    );
+    // Instantiatioons
+	cpu cpu(clk, reset, PC, Instr, MemWrite, DataAdr,WriteData, ReadData);
+	imem imem(PC, Instr);
+	dmem dmem(clk, MemWrite, DataAdr, WriteData, ReadData);
+ 
 
 endmodule
