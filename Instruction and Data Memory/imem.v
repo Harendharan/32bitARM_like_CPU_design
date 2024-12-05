ef745e67b86a865df79973067793b309cbb6e1dc
Harendharan B <93971342+Harendharan@users.noreply.github.com>
2024-12-05 12:24:19 +0530

    Update imem.v

--- Instruction and Data Memory/imem.v
+++ Instruction and Data Memory/imem.v
@@ -1,13 +1,11 @@
-module imem (
-    input [31:0] a,
-    output reg [31:0] rd
+module imem(
+  input logic [31:0] a,
+  output wire [31:0] rd
 );
-    
-    reg [31:0] RAM[63:0]; // 64 X 32
-
-    initial begin
-         $readmemh("memfile.dat", RAM);
-         rd = RAM[a[31:2]]; 
-    end
-
+  reg [31:0] RAM[63:0]; // 62 X 32
+  initial begin
+	$readmemh("memfile.dat",RAM);
+   end
+   assign rd = RAM[a[31:2]]; 
 endmodule
+
